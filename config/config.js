46aff442916fa52fb1ea64e48e11a9de312e97b0
sdc02 <sdc02@naver.com>
2020-02-27 21:23:54 +0900

    d

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   'authLogin':{
     'naver':{
-      'client_id':'process.env.NAVER_CLIENT_ID',
-      'secret_id':'process.env.NAVER_CLIENT_SECRET',
+      'client_id':process.env.NAVER_CLIENT_ID,
+      'secret_id':process.env.NAVER_CLIENT_SECRET,
       'callback_url':'https://picudream.herokuapp.com'
     }
   }
